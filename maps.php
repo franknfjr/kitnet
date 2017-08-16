a771110681486d611aa9373c6b2ad29f3726374e
Frank <frankdonascimentojr@gmail.com>
2017-08-16 19:43:08 -0300

    Update maps.php

--- maps.php
+++ maps.php
@@ -1,14 +1,15 @@
 <?php
-$address = 'universidade+federal+rural+da+amazonia,belem,para,brasil';
-$url = "http://maps.google.com/maps/api/geocode/json?address='.$address.'&sensor=false";
+$address = 'universidade federal rural da amazonia';
+$prepAddr = str_replace(' ','+',$address);
+$url = "http://maps.google.com/maps/api/geocode/json?address='.$prepAddr.'&sensor=false";
 $response = file_get_contents($url);
 $response = json_decode($response, true);
- 
+
 //print_r($response);
- 
+
 $lat = $response['results'][0]['geometry']['location']['lat'];
 $long = $response['results'][0]['geometry']['location']['lng'];
- 
+
 echo "latitude: " . $lat . " longitude: " . $long;
 ?>
 <!DOCTYPE html>
@@ -20,13 +21,9 @@ echo "latitude: " . $lat . " longitude: " . $long;
         width: 100%;
        }
     </style>
-  </head>
-  <body>
-    <h3>My Google Maps Demo</h3>
-    <div id="map"></div>
-    <script>
+	    <script>
       function initMap() {
-        var uluru = {lat: <?php echo $lat;?>, lng: <?php echo $long; ?> }; 
+        var uluru = {lat: <?php echo $lat;?>, lng: <?php echo $long; ?> };
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 15,
           center: uluru
@@ -37,8 +34,14 @@ echo "latitude: " . $lat . " longitude: " . $long;
         });
       }
     </script>
-    <script async defer
+	    <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDH-bx6ifyLYtMkY3jX5Vfa6M7BsUsW3nc&callback=initMap">
     </script>
+  </head>
+  <body>
+    <h3>My Google Maps Demo</h3>
+    <div id="map"></div><br>
+	   <?php echo "<a href='view.php'>Voltar</a>" ?>
+
   </body>
 </html>
