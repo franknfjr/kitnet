fd3d0dbf60d91149dc8635ff5f7e5c233d7762a1
Frank <frankdonascimentojr@gmail.com>
2017-08-16 20:01:58 -0300

    Update maps.php

--- maps.php
+++ maps.php
@@ -5,7 +5,6 @@ $url = "http://maps.google.com/maps/api/geocode/json?address='.$prepAddr.'&senso
 $response = file_get_contents($url);
 $response = json_decode($response, true);
 
-//print_r($response);
 
 $lat = $response['results'][0]['geometry']['location']['lat'];
 $long = $response['results'][0]['geometry']['location']['lng'];
