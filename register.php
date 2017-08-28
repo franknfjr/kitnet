de9edf8de580d66ba5476bfd549e6d5bc03b22e7
Frank <frankdonascimentojr@gmail.com>
2017-08-28 16:52:16 -0300

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<title>Register</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 </head>
 
 <body>
@@ -10,71 +11,80 @@ include("connection.php");
 
 if(isset($_POST['submit'])) {
 	$name = $_POST['name'];
-    $address = $_POST['address'];
-    $pnumber = $_POST['pnumber'];
-    $rg = $_POST['rg'];
-    $cpf = $_POST['cpf'];
+        $address = $_POST['address'];
+        $pnumber = $_POST['pnumber'];
+        $rg = $_POST['rg'];
+        $cpf = $_POST['cpf'];
 	$email = $_POST['email'];
 	$user = $_POST['username'];
 	$pass = $_POST['password'];
 
 	if($name == "" || $address == "" || $pnumber == "" || $rg == "" || $cpf == "" || $email == "" || $user == "" || $pass == ""){
-		echo "All fields should be filled. Either one or many fields are empty.";
+		echo "Todos os campos devem ser preenchidos. Um ou varios campos estao vazios.";
 		echo "<br/>";
-		echo "<a href='register.php'>Go back</a>";
+		echo "<a href='register.php'>Voltar</a>";
 	} else {
 		mysqli_query($mysqli, "INSERT INTO proprietario(nome, endereco , telefone , rg, cpf, email, username, senha) VALUES('$name', '$address', '$pnumber', '$rg', '$cpf', '$email', '$user', md5('$pass'))")
 			or die("Could not execute the insert query.");
 			
-		echo "Registration successfully";
+		echo "Cadastrado com sucesso!";
 		echo "<br/>";
 		echo "<a href='login.php'>Login</a>";
 	}
 } else {
-?>
-	<p><font size="+2">Register</font></p>
-	<form name="form1" method="post" action="">
-		<table width="75%" border="0">
-			<tr> 
-				<td width="10%">Full Name</td>
-				<td><input type="text" name="name"></td>
-			</tr>
-            <tr>
-                <td width="10%">Address</td>
-                <td><input type="text" name="address"></td>
-            </tr>
-            <tr>
-                <td width="10%">Phone number</td>
-                <td><input type="text" name="pnumber"></td>
-            </tr>
-            <tr>
-                <td>Rg</td>
-                <td><input type="text" name="rg"></td>
-            </tr>
-            <tr>
-                <td>Cpf</td>
-                <td><input type="text" name="cpf"></td>
-            </tr>
-			<tr> 
-				<td>Email</td>
-				<td><input type="text" name="email"></td>
-			</tr>			
-			<tr> 
-				<td>Username</td>
-				<td><input type="text" name="username"></td>
-			</tr>
-			<tr> 
-				<td>Password</td>
-				<td><input type="password" name="password"></td>
-			</tr>
-			<tr> 
-				<td>&nbsp;</td>
-				<td><input type="submit" name="submit" value="Submit"></td>
-			</tr>
-		</table>
-	</form>
-<?php
-}
+?>	  <div class="col-md-6 offset-md-3">
+                    
+                    <hr class="mb-5">
+
+                    <!-- form card register -->
+                    <div class="card card-outline-secondary">
+                        <div class="card-header">
+                            <h3 class="mb-0">Sign Up</h3>
+                        </div>
+                        <div class="card-block">
+                            <form class="form" role="form" autocomplete="off" name="form1" method="post" action="">
+                                <div class="form-group">
+                                    <label for="inputName">Name</label>
+                                    <input type="text" class="form-control" name="name" placeholder="Joao Pedro Cruz">
+                                </div>
+						 <div class="form-group">
+                                    <label for="inputName">Endereco</label>
+                                    <input type="text" class="form-control" name="address" placeholder="ex: Universidade Federal Rural da Amazonia">
+                                </div>
+						 <div class="form-group">
+                                    <label for="inputName">Telefone</label>
+                                    <input type="text" class="form-control" name="pnumber" placeholder="ex: 982457414">
+                                </div>
+						 <div class="form-group">
+                                    <label for="inputName">RG</label>
+                                    <input type="text" class="form-control" name="rg" placeholder="ex: 5545333">
+                                </div>
+						 <div class="form-group">
+                                    <label for="inputName">C.P.F</label>
+                                    <input type="text" class="form-control" name="cpf" placeholder="ex: 01247454522">
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputEmail3">Email</label>
+                                    <input type="email" class="form-control" name="email" placeholder="ex: ufra@edu.br" required="">
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputPassword3">Apelido</label>
+                                    <input type="text" class="form-control" name="username" placeholder="ex: 4head" required="">
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputVerify3">Senha</label>
+                                    <input type="password" class="form-control" name="password" placeholder=" ****** " required="">
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" name="submit" value="Submit" class="btn btn-success btn-lg float-right">Register</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- /form card register -->
+                </div>
+	<?php
+    }
 ?>
 </body>
 </html>
