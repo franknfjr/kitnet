f6f63c7f665a9ff45300cdd2fc557e159fca23e9
Frank <frankdonascimentojr@gmail.com>
2017-08-28 16:24:39 -0300

    Update login.php

--- login.php
+++ login.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>Login</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 </head>
 
 <body>
@@ -13,11 +14,7 @@ if(isset($_POST['submit'])) {
     $user = mysqli_real_escape_string($mysqli, $_POST['username']);
     $pass = mysqli_real_escape_string($mysqli, $_POST['password']);
 
-    if($user == "" || $pass == "") {
-        echo "Either username or password field is empty.";
-        echo "<br/>";
-        echo "<a href='login.php'>Go back</a>";
-    } else {
+     if( $user != null && $pass !=null){
         $result = mysqli_query($mysqli, "SELECT * FROM proprietario WHERE username='$user' AND senha=md5('$pass')")
         or die("Could not execute the select query.");
 
@@ -29,9 +26,9 @@ if(isset($_POST['submit'])) {
                 $_SESSION['nome'] = $row['nome'];
                 $_SESSION['id'] = $row['id'];
         } else {
-            echo "Invalid username or password.";
+            echo "Usuario ou senha invalidos.";
             echo "<br/>";
-            echo "<a href='login.php'>Go back</a>";
+            echo "<a href='login.php'>Voltar</a>";
         }
 
         if(isset($_SESSION['valid'])) {
@@ -40,23 +37,35 @@ if(isset($_POST['submit'])) {
     }
 } else {
     ?>
-    <p><font size="+2">Login</font></p>
-    <form name="form1" method="post" action="">
-        <table width="75%" border="0">
-            <tr>
-                <td width="10%">Username</td>
-                <td><input type="text" name="username"></td>
-            </tr>
-            <tr>
-                <td>Password</td>
-                <td><input type="password" name="password"></td>
-            </tr>
-            <tr>
-                <td>&nbsp;</td>
-                <td><input type="submit" name="submit" value="Submit"></td>
-            </tr>
-        </table>
-    </form>
+            <hr class="mb-5">
+            <div class="col" align="center">
+                <div class="col-md-6 offset-md-3" >
+ <!-- form card login -->
+                    <div class="card card-outline-secondary">
+                        <div class="card-header">
+                            <h3 class="mb-0">Login</h3>
+                        </div>
+                        <div class="card-block">
+                            <form name="form1" method="post" action="" role="form" autocomplete="off">
+                                <div class="form-group">
+                                    <label for="uname1">Username</label>
+                                    <input type="text" class="form-control" name="username" id="uname1" required="">
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input type="password" class="form-control" name="password" required="" autocomplete="new-password">
+                                </div>
+                                                                <button type="submit" name="submit" value="Submit" class="btn btn-success btn-lg float-right">Login</button>
+                            </form>
+                        </div>
+                        <!--/card-block-->
+                    </div>
+                    <!-- /form card login -->
+                </div>
+         </div>
+   </div>
+    
+  
     <?php
 }
 ?>
