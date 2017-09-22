5a266521ee58ce97f96236e9dd3f6bfcdea4b8fd
stonetek <pelkorstone@gmail.com>
2017-09-22 18:51:43 -0300

    Add files via upload
    alteração para possibilitar cadastrar clientes, deletar clientes.

---
+++ registerCliente.php
@@ -0,0 +1,90 @@
+<html>
+   <head>
+      <title>Register</title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+   </head>
+   <body>
+      <a href="index.php">Home</a> <br />
+      <?php
+         include("connection.php");
+
+         if(isset($_POST['submit'])) {
+         	$name = $_POST['name'];
+          $address = $_POST['address'];
+          $pnumber = $_POST['pnumber'];
+          $rg = $_POST['rg'];
+          $cpf = $_POST['cpf'];
+          $email = $_POST['email'];
+          $user = $_POST['username'];
+          $pass = $_POST['password'];
+
+         	if($name == "" || $address == "" || $pnumber == "" || $rg == "" || $cpf == "" || $email == "" || $user == "" || $pass == ""){
+         		echo "Todos os campos devem ser preenchidos. Um ou varios campos estao vazios.";
+         		echo "<br/>";
+         		echo "<a href='registercliente.php'>Voltar</a>";
+         	} else {
+         		mysqli_query($mysqli, "INSERT INTO cliente(nome, endereco , telefone , rg, cpf, email, username, senha) VALUES('$name', '$address', '$pnumber', '$rg', '$cpf', '$email', '$user', md5('$pass'))")
+         			or die("Could not execute the insert query.");
+
+         		echo "Cadastrado com sucesso!";
+         		echo "<br/>";
+         		echo "<a href='login.php'>Login</a>";
+         	}
+         } else {
+         ?>
+      <hr class="mb-5">
+      <div class="col" align="center">
+         <div class="col-md-6 offset-md-3" >
+            <!-- form card login -->
+            <div class="card card-outline-secondary">
+               <div class="card-header">
+                  <h3 class="mb-0">Register</h3>
+               </div>
+               <div class="card-block">
+                  <form class="form" role="form" autocomplete="off" name="form1" method="post" action="">
+                     <div class="form-group">
+                        <label for="inputName">Name</label>
+                        <input type="text" class="form-control" name="name" placeholder="Joao Pedro Cruz">
+                     </div>
+                     <div class="form-group">
+                        <label for="inputName">Endereco</label>
+                        <input type="text" class="form-control" name="address" placeholder="ex: Universidade Federal Rural da Amazonia">
+                     </div>
+                     <div class="form-group">
+                        <label for="inputName">Telefone</label>
+                        <input type="text" class="form-control" name="pnumber" placeholder="ex: 982457414">
+                     </div>
+                     <div class="form-group">
+                        <label for="inputName">RG</label>
+                        <input type="text" class="form-control" name="rg" placeholder="ex: 5545333">
+                     </div>
+                     <div class="form-group">
+                        <label for="inputName">C.P.F</label>
+                        <input type="text" class="form-control" name="cpf" placeholder="ex: 01247454522">
+                     </div>
+                     <div class="form-group">
+                        <label for="inputEmail3">Email</label>
+                        <input type="email" class="form-control" name="email" placeholder="ex: ufra@edu.br" required="">
+                     </div>
+                     <div class="form-group">
+                        <label for="inputPassword3">Apelido</label>
+                        <input type="text" class="form-control" name="username" placeholder="ex: 4head" required="">
+                     </div>
+                     <div class="form-group">
+                        <label for="inputVerify3">Senha</label>
+                        <input type="password" class="form-control" name="password" placeholder=" ****** " required="">
+                     </div>
+                     <div class="form-group">
+                        <button type="submit" name="submit" value="Submit" class="btn btn-success btn-lg float-right">Register</button>
+                     </div>
+                  </form>
+               </div>
+            </div>
+            <!-- /form card registercliente -->
+         </div>
+      </div>
+      <?php
+         }
+         ?>
+   </body>
+</html>
---
+++ viewcliente.php
@@ -0,0 +1,90 @@
+<?php session_start(); ?>
+
+<?php
+if(!isset($_SESSION['valid'])) {
+	header('Location: login.php');
+}
+?>
+
+<?php
+//including the database connection file
+include_once("connection.php");
+
+//fetching data in descending order (lastest entry first)
+$result = mysqli_query($mysqli, "SELECT * FROM cliente WHERE id=".$_SESSION['id']." ORDER BY id DESC");
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="icon" href="https://getbootstrap.com/favicon.ico">
+      <title>Kitnet</title>
+      <!-- Bootstrap core CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+      <!-- Custom styles for this template -->
+      <link href="narrow-jumbotron.css" rel="stylesheet">
+   </head>
+   <body>
+		 <div class="container">
+	          <div class="header clearfix">
+	             <nav>
+	                <ul class="nav nav-pills float-right" role="tablist">
+	                   <li class="nav-item">
+	                      <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+	                   </li>
+	                   <li class="nav-item">
+	                      <a class="nav-link" href="add.html">Add Imovel</a>
+	                   </li>
+										 <li class="nav-item">
+												<a class="nav-link" href="viewcliente.php">Ajustes</a>
+										 </li>
+	                   <li class="nav-item">
+	                      <a class="nav-link" href="logout.php">Logout</a>
+	                   </li>
+	                </ul>
+	             </nav>
+	             <h3 class="text-muted">Kitnet</h3>
+	          </div>
+<table class="table">
+  <thead class="thead-inverse">
+    <tr>
+      <th>Nome</th>
+      <th>Endereço</th>
+      <th>Telefone</th>
+      <th>RG</th>
+      <th>CPF</th>
+      <th>Email</th>
+      <th>Username</th>
+      <th>Senha</th>
+      <th>Update</th>
+    </tr>
+  </thead>
+  <tbody>
+			<?php
+			while($res = mysqli_fetch_array($result)) {
+				echo "<tr>";
+				echo "<td>".$res['nome']."</td> ";
+				echo "<td>".$res['endereco']."</td>";
+				echo "<td>".$res['telefone']."</td>";
+				echo "<td>".$res['rg']."</td>";
+				echo "<td>".$res['cpf']."</td>";
+				echo "<td>".$res['email']."</td>";
+				echo "<td>".$res['username']."</td>";
+				echo "<td>".$res['senha']."</td>";
+ 				echo "<td><a class='btn btn-primary' href=\"editcliente.php?id=$res[id]\">Edit</a> | <a class='btn btn-danger' href=\"deletecliente.php?id=$res[id]\" onClick=\"return confirm('Deletando sua conta voce removerá todos seus registros do banco de dados. Voce tem certeza?')\">Delete</a></td>";
+			}
+			?>
+		</tr>
+  </tbody>
+</table>
+</div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  </body>
+</html>
